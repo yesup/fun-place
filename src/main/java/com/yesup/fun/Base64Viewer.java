c6f97d58721e7aa58dbfb474ccab61f52f4810ee
Jeff Ye <jeffyecn@gmail.com>
2017-07-11 14:40:50 -0400

    base 64 decoder

---
+++ src/main/java/com/yesup/fun/Base64Viewer.java
@@ -0,0 +1,54 @@
+package com.yesup.fun;
+
+import com.yesup.as.AddRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Base64;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+
+/**
+ * Created by jeffye on 11/07/17.
+ */
+public class Base64Viewer {
+
+    private final static Logger LOG;
+
+    static {
+        // configure log4j
+        org.apache.log4j.PropertyConfigurator.configure(Constants.LOG_CONF);
+
+        // silent java logging of library
+        for (Handler h : LogManager.getLogManager().getLogger("").getHandlers() ) {
+            h.setLevel(Level.SEVERE);
+        }
+
+        LOG = LoggerFactory.getLogger(AddRecord.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        if (args.length == 0 ) {
+            LOG.error("missing string as input");
+            return;
+        }
+
+        byte[] bytes = Base64.getDecoder().decode(args[0]);
+        for(int i=0; i<bytes.length; i=i+8) {
+            String str = "";
+            for (int j=i; j<bytes.length && j<i+8; j++) {
+                byte one = bytes[j];
+                System.out.print(String.format("%02X", one));
+                System.out.print(" ");
+                if ( one >= 32 && one < 127 ) {
+                    char c = (char)(one);
+                    str = str + c;
+                } else {
+                    str = str + ".";
+                }
+            }
+            System.out.println(str);
+        }
+    }
+}
